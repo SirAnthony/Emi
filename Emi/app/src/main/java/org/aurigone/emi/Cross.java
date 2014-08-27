250184d27d7a49efd9d4fc2358924811ed32621a
Sir Anthony <anthony@adsorbtion.org>
2014-08-28 00:20:38 +0600

    Fix view.

--- Emi/app/src/main/java/org/aurigone/emi/Cross.java
+++ Emi/app/src/main/java/org/aurigone/emi/Cross.java
@@ -2,8 +2,6 @@ package org.aurigone.emi;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
@@ -11,26 +9,21 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import org.aurigone.emi.CrossLib.ClockTextView;
 import org.aurigone.emi.CrossLib.Item;
 import org.aurigone.emi.CrossLib.Schedule;
 import org.aurigone.emi.CrossLib.State;
 import org.aurigone.emi.CrossLib.Task;
-import org.w3c.dom.ls.LSResourceResolver;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Iterator;
 
 
 public class Cross extends Activity implements ClockTextView.ActionSwitcher {
 
-    private static ClockTextView chronometer;
+    private ClockTextView chronometer;
     private Iterator<Item> taskIter;
 
     void load(State st) {
@@ -63,19 +56,12 @@ public class Cross extends Activity implements ClockTextView.ActionSwitcher {
         chronometer.setItem(action);
     }
 
-    public void updateTime(String text) {
-        final ClockTextView time = (ClockTextView) findViewById(R.id.TimeLeft);
-        time.setText(text);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         State state;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_cross);
-        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.fragment_cross, null);
-        chronometer = (ClockTextView ) view.findViewById(R.id.TimeLeft);
+        chronometer = (ClockTextView ) findViewById(R.id.TimeLeft);
         chronometer.setListener(this);
         Bundle extras = getIntent().getExtras();
         state = (State) extras.get("STATE");
@@ -99,5 +85,11 @@ public class Cross extends Activity implements ClockTextView.ActionSwitcher {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        chronometer.stop();
+        super.onBackPressed();
     }
 }
--- Emi/app/src/main/java/org/aurigone/emi/CrossLib/ClockTextView.java
+++ Emi/app/src/main/java/org/aurigone/emi/CrossLib/ClockTextView.java
@@ -4,11 +4,8 @@ import android.content.Context;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by anthony on 24.08.14.
@@ -17,14 +14,19 @@ public class ClockTextView extends TextView {
     final long interval = 1000;
     private ActionSwitcher listener;
     private ChronoTimer timer;
+    private Handler handler = new Handler();
 
     public ClockTextView(Context c) { super(c); }
     public ClockTextView(Context c, AttributeSet as) { super(c, as); }
     public ClockTextView(Context c, AttributeSet as, int d) { super(c, as, d); }
 
+    public void stop() {
+        timer.cancel();
+        timer = null;
+    }
+
     public interface ActionSwitcher {
         public void actionFinished();
-        public void updateTime(String s);
     }
 
     private class ChronoTimer extends CountDownTimer {
@@ -34,7 +36,10 @@ public class ClockTextView extends TextView {
         @Override
         public void onFinish() {
             updateText(0);
-            listener.actionFinished();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() { listener.actionFinished(); }
+            }, 1500);
         }
 
         @Override
@@ -56,7 +61,9 @@ public class ClockTextView extends TextView {
 
     void updateText(long timeLeft){
         long time = timeLeft / interval;
-        String stime = String.format("%02d:%02d", time / 60, time % 60);
-        listener.updateTime(stime);
+        final String sstime = String.format("%02d:%02d", time / 60, time % 60);
+        setText(sstime);
     }
+
+
 }
